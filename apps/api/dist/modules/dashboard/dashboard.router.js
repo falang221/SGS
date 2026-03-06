b3db333689ea4d66ac3a04a21458bd42a9d8a900
falang221 <ndiayebuziness@gmail.com>
2026-03-06 22:13:10 +0000

    chore(git): stop tracking generated artifacts

--- apps/api/dist/modules/dashboard/dashboard.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboard_controller_1 = require("./dashboard.controller");
-const auth_middleware_1 = require("../../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.use(auth_middleware_1.authMiddleware);
-router.get('/stats', dashboard_controller_1.DashboardController.getStats);
-exports.default = router;
